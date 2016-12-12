80642a7accaf11bd84d309c8af047738599cf08c
CCAZ88 <camcardosi@mac.com>
2016-12-12 12:41:46 -0700

    added gitignore and removed node modules

--- models/unit.js
+++ models/unit.js
@@ -3,21 +3,21 @@ var Schema = mongoose.Schema;
 var unitSchema = new Schema({
   contNum: Number,
   brand: String,
-  // status: String,
-  // location: String,
-  // sub_location: String,
-  // debt: Number,
-  // monthly_cost: Number,
-  // current_monthly_revenue: Number,
-  // monthly_net: Number,
-  // pay_off_date: String,
-  // revenue_to_date: Number,
-  // net_to_date: Number,
-  // current_customer: String,
-  // next_delivery: String,
-  // transaction_history: String,
-  // delivery_history: String,
-  // customer_history: String
+  status: String,
+  location: String,
+  sub_location: String,
+  debt: Number,
+  monthly_cost: Number,
+  current_monthly_revenue: Number,
+  monthly_net: Number,
+  pay_off_date: String,
+  revenue_to_date: Number,
+  net_to_date: Number,
+  current_customer: String,
+  next_delivery: String,
+  transaction_history: String,
+  delivery_history: String,
+  customer_history: String
 });
 
 var Unit = mongoose.model('Unit', unitSchema);
